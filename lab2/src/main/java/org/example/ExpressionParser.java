93c835eed233b3eed35c4960446e6b97d1193b65
Анна <@DESKTOP-QFAA5GG>
2025-03-20 18:46:34 +0300

    add parse expression

--- lab2/src/main/java/org/example/ExpressionParser.java
+++ lab2/src/main/java/org/example/ExpressionParser.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.Map;
 import java.util.Stack;
 
 /**
@@ -16,6 +17,32 @@ public class ExpressionParser {
                 return true;
         }
         return false;
+    }
+
+    private static int getOperatorPriority(Character ch) {
+        switch(ch) {
+            case '+':
+            case '-':
+                return 1;
+            case '*':
+            case '/':
+                return 2;
+        }
+        return 0;
+    }
+
+    private static double applyOperator(Character ch, double num2, double num1) {
+        switch(ch) {
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            case '*':
+                return num1 * num2;
+            case '/':
+                return num1 / num2;
+        }
+        return 0;
     }
 
     /**
@@ -103,4 +130,70 @@ public class ExpressionParser {
         return nextMember ? -1 : i;
     }
 
+    public static double parseExpression(String expression, Map<String, Double> variables) {
+        if (!isCorrectExpression(expression)) {
+            throw new IllegalArgumentException("Expression not correct");
+        }
+        return parseExpression(expression, variables, 0).value();
+    }
+
+    private static ResultPair parseExpression(String expression, Map<String, Double> variables, int i) {
+        boolean nextMember = true;
+        Stack<Double> numbers = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+        Function f;
+        while(i < expression.length() && expression.charAt(i) != ')') {
+            if (nextMember) {
+                if (expression.charAt(i) != '(') {
+                    StringBuilder memberBuilder = new StringBuilder();
+                    while (i < expression.length() && isMemberContainsChar(expression.charAt(i))) {
+                        memberBuilder.append(expression.charAt(i++));
+                    }
+                    if ((f = Function.getFunction(memberBuilder.toString())) != null) {
+                        i++;//skip '('
+                        ResultPair result = parseExpression(expression, variables, i);
+                        i = result.index();
+                        numbers.push(result.value());
+                        i++;//skip ')'
+                    } else if (Character.isLetter(memberBuilder.charAt(0))) {
+                        Double value = variables.get(memberBuilder.toString());
+                        if (value == null) {
+                            throw new IllegalArgumentException(String.format("Variable not found - %s",
+                                    memberBuilder.toString()));
+                        }
+                        numbers.push(value);
+                    } else {
+                        numbers.push(Double.parseDouble(memberBuilder.toString()));
+                    }
+                } else {
+                    ++i;//skip '('
+                    ResultPair result = parseExpression(expression, variables, i);
+                    i = result.index();
+                    numbers.push(result.value());
+                    i++;//skip ')'
+                }
+                nextMember = false;
+            } else {
+                i = skipSpaces(expression, i);
+
+                Character operator = expression.charAt(i++);
+                if (!operators.isEmpty() &&
+                        getOperatorPriority(operators.peek()) >= getOperatorPriority(operator)) {
+                    numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+                }
+                operators.push(operator);
+                i = skipSpaces(expression, i);
+                nextMember = true;
+            }
+        }
+
+        while (!operators.isEmpty()) {
+            numbers.push(applyOperator(operators.pop(), numbers.pop(), numbers.pop()));
+        }
+
+        return new ResultPair(numbers.pop(), i);
+    }
+
+    private record ResultPair(double value, int index) {
+    }
 }
--- lab2/src/test/java/org/example/ExpressionParserTest.java
+++ lab2/src/test/java/org/example/ExpressionParserTest.java
@@ -3,27 +3,52 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class ExpressionParserTest {
 
     @Test
     void testIsCorrectExpression() {
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression(""), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("1"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("1 + "), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("1 2"), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("1.5 + 2"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("(1 + 2)"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("(1+2)"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("(1 + 2))"), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("((1 * 2)"), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("((1 + 2) + (1 + 2))"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("(1/cos(0))"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("cos(sin(tg(ctg(1))))"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("x + 1"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("x - y"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("x + 145 + 13 * (y + cos(z))"), true);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("x * "), false);
-        Assertions.assertEquals(ExpressionParser.isCorrectExpression("x - y.y"), false);
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression(""));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("1"));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("1 + "));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("1 2"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("1.5 + 2"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("(1 + 2)"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("(1+2)"));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("(1 + 2))"));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("((1 * 2)"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("((1 + 2) + (1 + 2))"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("(1/cos(0))"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("cos(sin(tg(ctg(1))))"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("x + 1"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("x - y"));
+        Assertions.assertEquals(true, ExpressionParser.isCorrectExpression("x + 145 + 13 * (y + cos(z))"));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("x * "));
+        Assertions.assertEquals(false, ExpressionParser.isCorrectExpression("x - y.y"));
     }
 
+    @Test
+    void testParseExpression() {
+        Assertions.assertEquals(3.4, ExpressionParser.parseExpression("1 + 2.4", null));
+        Assertions.assertEquals(9.0, ExpressionParser.parseExpression("1 + 1.5*4 + 2", null));
+        Assertions.assertEquals(21.0, ExpressionParser.parseExpression("1 + 2*2 * 5", null));
+        Assertions.assertEquals(1.0, ExpressionParser.parseExpression("1 + cos(0)", null));
+        Assertions.assertEquals(35.0, ExpressionParser.parseExpression("2 * (1 + 4) + (5 * (2 + 3) + cos(0))", null));
+    }
+
+    @Test
+    void testParseExpressionWithVariables() {
+        Map<String, Double> variables = new HashMap<>();
+        variables.put("x", 1.0);
+        variables.put("y", 2.0);
+        variables.put("z", 3.0);
+        variables.put("a", 0.0);
+
+        Assertions.assertEquals(3.0, ExpressionParser.parseExpression("x + y", variables));
+        Assertions.assertEquals(2.0, ExpressionParser.parseExpression("y/x", variables));
+        Assertions.assertEquals(0.0, ExpressionParser.parseExpression("(x + y) * cos(a)", variables));
+        Assertions.assertEquals(9.0, ExpressionParser.parseExpression("(x + y) * (z - a)", variables));
+    }
 }
